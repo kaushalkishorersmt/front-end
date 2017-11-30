3c0111a2b3136e3c46d4b1c536d38b65d2fd29b2
kaushalkishorersmt <kaushalkishorersmt@gmail.com>
2017-11-30 14:50:29 +0530

    Implemented interface to simulate the API.

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Document } from './document'
 @Component({
   selector: 'app-documents',
   templateUrl: './documents.component.html',
@@ -11,5 +11,30 @@ export class DocumentsComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  pageTitle: string = "Document Dashboard"
+  documents: Document[] = [
+    {
+      title: "My first Doc",
+      description: 'This is the description of doc.',
+      file_url: 'http://google.com',
+      updated_at: '27/11/2017',
+      image_url: 'http://google.com',
+    },
+    {
+      title: "My Second Doc",
+      description: 'This is the description of doc.',
+      file_url: 'http://google.com',
+      updated_at: '27/11/2017',
+      image_url: 'http://google.com',
+    },
+    {
+      title: "My Last Doc",
+      description: 'This is the description of doc.',
+      file_url: 'http://google.com',
+      updated_at: '27/11/2017',
+      image_url: 'http://google.com',
+    }
+  ]
 
 }
