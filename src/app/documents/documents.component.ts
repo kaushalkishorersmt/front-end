9dc0cdc1f7d3da308d7a976031f8a34e78598a26
kaushalkishorersmt <kaushalkishorersmt@gmail.com>
2017-12-02 14:05:30 +0530

    Add card style for documents.

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -19,21 +19,21 @@ export class DocumentsComponent implements OnInit {
       description: 'This is the description of doc.',
       file_url: 'http://google.com',
       updated_at: '27/11/2017',
-      image_url: 'http://google.com',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
     },
     {
       title: "My Second Doc",
       description: 'This is the description of doc.',
       file_url: 'http://google.com',
       updated_at: '27/11/2017',
-      image_url: 'http://google.com',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
     },
     {
       title: "My Last Doc",
       description: 'This is the description of doc.',
       file_url: 'http://google.com',
       updated_at: '27/11/2017',
-      image_url: 'http://google.com',
+      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
     }
   ]
 
