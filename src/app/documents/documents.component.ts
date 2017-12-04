eba7eb9f169a5e5e6b2f70dbca06fe046a0fa57e
kaushalkishorersmt <kaushalkishorersmt@gmail.com>
2017-12-04 11:16:48 +0530

    Setup the app module with http for API calls, setup the document serive and document component to accept the service.

--- src/app/documents/documents.component.ts
+++ src/app/documents/documents.component.ts
@@ -1,40 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-import { Document } from './document'
+import { Observable } from 'rxjs/Rx';
+import { Document } from './document';
+import { DocumentService } from './document.service';
+
 @Component({
   selector: 'app-documents',
   templateUrl: './documents.component.html',
-  styleUrls: ['./documents.component.css']
+  styleUrls: ['./documents.component.css'],
+  providers: [ DocumentService ]
 })
 export class DocumentsComponent implements OnInit {
+  pageTitle: string = "Document Dashboard"
+  documents: Document[];
+  errorMessage: string;
+  mode = "Observable";
 
-  constructor() { }
+  constructor(
+    private documentService: DocumentService,
+  ) { }
 
   ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getDocuments());
   }
 
-  pageTitle: string = "Document Dashboard"
-  documents: Document[] = [
-    {
-      title: "My first Doc",
-      description: 'This is the description of doc.',
-      file_url: 'http://google.com',
-      updated_at: '27/11/2017',
-      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
-    },
-    {
-      title: "My Second Doc",
-      description: 'This is the description of doc.',
-      file_url: 'http://google.com',
-      updated_at: '27/11/2017',
-      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
-    },
-    {
-      title: "My Last Doc",
-      description: 'This is the description of doc.',
-      file_url: 'http://google.com',
-      updated_at: '27/11/2017',
-      image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
-    }
-  ]
+  getDocuments() {
+    this.documentService.getDocuments()
+        .subscribe(
+          documents => this.documents = documents,
+          error => this.errorMessage = <any>error
+        );
+  }
+
+
+  // documents: Document[] = [
+  //   {
+  //     title: "My first Doc",
+  //     description: 'This is the description of doc.',
+  //     file_url: 'http://google.com',
+  //     updated_at: '27/11/2017',
+  //     image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
+  //   },
+  //   {
+  //     title: "My Second Doc",
+  //     description: 'This is the description of doc.',
+  //     file_url: 'http://google.com',
+  //     updated_at: '27/11/2017',
+  //     image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
+  //   },
+  //   {
+  //     title: "My Last Doc",
+  //     description: 'This is the description of doc.',
+  //     file_url: 'http://google.com',
+  //     updated_at: '27/11/2017',
+  //     image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Mistakes-to-avoid-when-hiring-freelancers.jpg',
+  //   }
+  // ]
 
 }
